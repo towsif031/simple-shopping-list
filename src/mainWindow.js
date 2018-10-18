1348d478f08662dd12b5a9f3c5fc5d85c39f9f8c
Towsif Ahmed Omi <towsif031@gmail.com>
2018-10-19 00:14:29 +0600

    Added style to mainWindow

--- src/mainWindow.js
+++ src/mainWindow.js
@@ -4,7 +4,9 @@ const ul = document.querySelector('ul');
 
 // Add item
 ipcRenderer.on('item:add', function(event, item) {
+    ul.className = 'collection';
     const li = document.createElement('li');
+    li.className = 'collection-item';
     const itemText = document.createTextNode(item);
     li.appendChild(itemText);
     ul.appendChild(li);
@@ -13,6 +15,7 @@ ipcRenderer.on('item:add', function(event, item) {
 // Clear items
 ipcRenderer.on('item:clear', function() {
     ul.innerHTML = '';
+    ul.className = '';
 });
 
 // Remove item
@@ -20,4 +23,7 @@ ul.addEventListener('dblclick', removeItem);
 
 function removeItem(event) {
     event.target.remove();
+    if(ul.children.length == 0) {
+        ul.className = '';
+    }
 }
