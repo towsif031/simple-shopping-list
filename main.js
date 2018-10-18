dc90833f30e292a988ee5323554bd8c3e7ed06ee
Towsif Ahmed Omi <towsif031@gmail.com>
2018-10-18 23:56:22 +0600

    Added clear_items and remove_item function

--- main.js
+++ main.js
@@ -66,7 +66,10 @@ const mainMenuTemplate = [
                 }
             },
             {
-                label: 'Clear Items'
+                label: 'Clear Items',
+                click() {
+                    mainWindow.webContents.send('item:clear');
+                }
             },
             {
                 label: 'Quit',
--- src/mainWindow.js
+++ src/mainWindow.js
@@ -2,9 +2,22 @@ const electron = require('electron');
 const {ipcRenderer} = electron;
 const ul = document.querySelector('ul');
 
-ipcRenderer.on('item:add', function (event, item) {
+// Add item
+ipcRenderer.on('item:add', function(event, item) {
     const li = document.createElement('li');
     const itemText = document.createTextNode(item);
     li.appendChild(itemText);
     ul.appendChild(li);
 });
+
+// Clear items
+ipcRenderer.on('item:clear', function() {
+    ul.innerHTML = '';
+});
+
+// Remove item
+ul.addEventListener('dblclick', removeItem);
+
+function removeItem(event) {
+    event.target.remove();
+}
